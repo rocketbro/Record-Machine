b88d534fcd7b5376248e4062bb0b702cf948e8d7
Asher Pope <asherpope@gmail.com>
2024-01-03 13:34:32 -0500

    Initial Commit

---
+++ RecordMachine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RecordMachine
+//
+//  Created by Asher Pope on 1/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RecordMachine/RecordMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecordMachineApp.swift
+//  RecordMachine
+//
+//  Created by Asher Pope on 1/3/24.
+//
+
+import SwiftUI
+
+@main
+struct RecordMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
