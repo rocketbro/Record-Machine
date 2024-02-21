8f287d1b70d58f9e158529a5cece958c0bd187a9
rocketbro <asherpope@gmail.com>
2024-02-20 20:00:16 -0500

    Add background audio capabilities

--- RecordMachine/RecordMachineApp.swift
+++ RecordMachine/RecordMachineApp.swift
@@ -7,14 +7,29 @@
 
 import SwiftUI
 import SwiftData
+import AVFoundation
 
 @main
 struct RecordMachineApp: App {
+    init() {
+        configureAudioSession()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .monospaced()
                 .preferredColorScheme(.dark)
         }.modelContainer(for: Album.self)
+    }
+    
+    private func configureAudioSession() {
+        do {
+            let session = AVAudioSession.sharedInstance()
+            try session.setCategory(.playback, mode: .default, options: [.allowAirPlay, .allowBluetooth, .allowBluetoothA2DP, .mixWithOthers, .defaultToSpeaker])
+            try session.setActive(true)
+        } catch {
+            print("Error setting audio session:", error.localizedDescription)
+        }
     }
 }
